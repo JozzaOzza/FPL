5c674bb01adbacc590bea57152fbda6c735077be
JozzaOzza <JamieOrr13@gmail.com>
2022-09-13 12:40:37 +0200

    prints keys

--- get-keys.py
+++ get-keys.py
@@ -1,10 +1,13 @@
 # Set Python Interpreter to Anaconda one recommended with ctrl-shift-p
 
+# imports
 import requests
 import pandas as pd
 import numpy as np
 
+# testing requests
 url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
 r = requests.get(url)
-
 print(r.status_code)
+json = r.json()
+print(json.keys())
